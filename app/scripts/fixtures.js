449b7a5463f733c9da0c15468976b533510adb90
ckrgz001 <ckrgz001@outlook.com>
2017-08-23 20:08:12 -0400

    half of assignment

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,47 @@
+// Example Album
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+    ]
+ };
+
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+//  My submission for: assignment-11-DOM-Scripting-Album-View
+var albumFaves = {
+    title: 'Favorite Rock',
+    artist: 'Various Artists',
+    label: 'Universal',
+    year: '1970s ++',
+    albumArtUrl: 'assets/images/album_covers/cat.jpg',
+    songs: [
+        { title: 'Thunderstruck', duration: '4:52' },
+        { title: 'Black Dog', duration: '4:55' },
+        { title: 'Paint It Black', duration: '3:22'},
+        { title: 'The Rover', duration: '5:39' },
+        { title: 'Trampled Underfoot', duration: '5:15'}
+    ]
+};
