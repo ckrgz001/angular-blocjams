f37e80d600725fe34d7c672895a73adcbf88c251
ckrgz001 <ckrgz001@outlook.com>
2017-08-27 19:57:09 -0400

    Checkpoint 6 work

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -2,6 +2,7 @@
     function AlbumCtrl(Fixtures){
         this.albumData = Fixtures.getAlbum();
     }
+    
     angular
         .module('blocJams')
         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
