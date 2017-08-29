c044a4d15c50661c760d7deebbfbf12410c6966c
ckrgz001 <ckrgz001@outlook.com>
2017-08-29 19:54:51 -0400

    Complete Checkpoint work

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,16 +1,60 @@
+//Our SongPlayer service should now contain:
+//two private attributes: currentSong and currentBuzzObject,
+//one private function: setSong,
+//and two public methods: SongPlayer.play and SongPlayer.pause.
+
 (function() {
      function SongPlayer() {
+
           var SongPlayer = {};
 
-          SongPlayer.play = function(song) {
-            var currentBuzzObject = new buzz.sound(song.audioUrl, {
-          formats: ['mp3'],
-          preload: true
-      });
+          var currentSong = null;
 
-      currentBuzzObject.play();
-  };
-          return SongPlayer;
+          //@desc Buzz object audio file
+          //@type {Object}
+          var currentBuzzObject = null;
+
+          //@function setSong
+          //@desc Stops currently playing song and loads new audio file as currentBuzzObject
+          //@param {Object} song
+          var setSong = function(song) {
+              if (currentBuzzObject) {
+                  currentBuzzObject.stop();
+                  currentSong.playing = null;
+                }
+
+                currentBuzzObject = new buzz.sound(song.audioUrl, {
+                    formats: ['mp3'],
+                    preload: true
+                });
+
+                currentSong = song;
+            };
+
+
+        SongPlayer.play = function(song) {
+              if (currentSong !== song) {
+
+
+                setSong(song);
+
+                currentBuzzObject.play();
+                song.playing = true;
+
+            } else if (currentSong === song) {
+                    if (currentBuzzObject.isPaused()) {
+                        currentBuzzObject.play();
+                    }
+                }
+            };
+
+        SongPlayer.pause = function(song) {
+                currentBuzzObject.pause();
+                song.playing = false;
+            };
+
+
+         return SongPlayer;
      }
 
      angular
